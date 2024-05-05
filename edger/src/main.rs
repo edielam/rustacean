d574078504e4d9061fac77318ee61d5092559367
edielam <edguard738@gmail.com>
2024-05-04 21:07:51 -0400

    building a simple cli app

--- edger/src/main.rs
+++ edger/src/main.rs
@@ -1,5 +1,13 @@
+Struct Cli {
+    pattern: String;
+    path: std::path::PathBuf, 
+}
 fn main() {
     let pattern = std::env::args().nth(1).expect("where is the pattern, bro?");
     let path = std::env::args().nth(2).expect("Can't find your file man");
+
+    let args = Cli{
+        
+    }
     println!("Okay your pattern is {} and your path is {}",pattern, path)
 }
