8835ee7eb7f5dd31fab390cb94d99852677078a3
edielam <edguard738@gmail.com>
2024-05-08 13:31:13 -0400

    used Buffer reader instead of read_to_string

--- edger/src/main.rs
+++ edger/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
 
     let args = Cli::parse();
     //let content = std::fs::read_to_string(&args.path).expect("Yo what's in this file");
-    let f = File::open(&args.path).expect("Yo what's in this file?");
+    let f = File::open(&args.path).expect("Yo where's your file?");
     let content = std::io::BufReader::with_capacity(1024,f);
     
     
